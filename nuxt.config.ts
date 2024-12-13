d06aefe898460d15bbb8b74ee83f3a4799fa9f58
Vasyl Zinchenko <vasyl.zinchenko@modern-expo.com>
2024-12-13 04:14:51 +0200

    Initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/task-board/', // Назва вашого репозиторію
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/icon']
-})
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/icon'],
+});
